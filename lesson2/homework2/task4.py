2a28d186d81502cccb51f4a2610c480b1b09fc86
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-06-24 22:43:03 +0300

    HW2: completed task4

---
+++ lesson2/homework2/task4.py
@@ -0,0 +1,9 @@
+# У вас есть группа гостей для вип ложи. Места в вип ложе все заняты этими
+# гостями. Есть группа гостей в общем зале и места в нем все еще есть.
+# Отобразите 2 группы гостей в коде.
+
+vip = ("Mai Hatsune", "Prince William", "Russell Crowe",
+       "Catherine Zeta-Jones", "Phil Ivey", "Phil Hellmuth", "Zhou Yong")
+
+simple = ["John Doe", "Marta Doe", "Aamir khan", "Abhinav Shukla",
+          "Abhishek Bachchan", "Ajay Devgan"]
