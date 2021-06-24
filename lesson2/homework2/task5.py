41f50d718389e840e01c39dd4097c0ce00a49b16
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-06-25 00:32:58 +0300

    HW2: completed task5

---
+++ lesson2/homework2/task5.py
@@ -0,0 +1,25 @@
+# У вас есть группа людей с неуникальными именами. Сформируйте список не
+# повторяющихся имен (для этой задачи нельзя использовать set. Если в списке
+# есть 2 джона нужно взять лишь одного из них. "John Dow", "John Dow",
+# "Marta Dow" => "John Dow", "Marta Dow")
+
+people = ["John Doe", "Marta Doe", "Aamir khan", "Abhinav Shukla",
+          "Abhishek Bachchan", "Ajay Devgan", "Abhishek Bachchan",
+          "Ajay Devgan"]
+
+# since I can not use set, it looks like I may try to use frozenset.
+# They are similar but have difference
+
+unique_people = list(frozenset(people))
+
+# print(unique_people)
+
+# second way in case I was wrong with frozenset
+
+unique_people2 = []
+
+for name in people:
+    if name not in unique_people2:
+        unique_people2.append(name)
+
+# print(unique_people2)
