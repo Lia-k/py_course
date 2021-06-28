8feee9ef9dfc48be4bcefedba484b872e218c659
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-06-28 17:07:44 +0300

    added lesson 2

---
+++ lesson2/dicts.py
@@ -0,0 +1,45 @@
+john = {
+    "name": "john",
+    "age": 32
+}
+
+marta = {
+    "name": "marta",
+    "age": 24,
+}
+
+some = {"gender": "Male"}
+
+# james = john.copy()
+# print(james)
+
+# print(john.pop("name"))
+# print(john)
+
+# john.clear()
+# print(john)
+
+# print(john.items())
+# key, value = john.items()
+# print(key, value)
+
+# print(john.keys())
+#
+# john.setdefault("gender", "True")
+
+# print(john.popitem())
+# print(john)
+
+# print({}.fromkeys(["name", "age", "gender"], 5))
+
+# print(tuple(john.values()))
+
+# print(john.get("gender"))
+# print(john["gender"])
+
+# john.update(some)
+# print(john)
+
+# print(john | some)
+# print({**john, **some})
+
---
+++ lesson2/tuples.py
@@ -0,0 +1,9 @@
+coordinats_1 = (1, 1, 2, 4)
+coordinats_2 = 1, 2, 3, 4
+
+# print(coordinats_1)
+# print(coordinats_2)
+
+# print(coordinats_1.count(1))
+# print(coordinats_1.index(2))
+coordinats_1
