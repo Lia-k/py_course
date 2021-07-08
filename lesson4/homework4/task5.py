91a52a0ecc41fea019477f46dbd16dfce890d0d6
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-07-08 22:24:12 +0300

    HW4: completed task5

---
+++ lesson4/homework4/task5.py
@@ -0,0 +1,11 @@
+# у вас есть текст разбейте текст по предложениям. В качестве результата вы
+# должны получить список из предложений.
+
+import re
+import codecs
+
+with codecs.open('text.txt', encoding='utf-8') as file:
+    text = file.read()
+    sentences = re.findall(r"[А-Я].*?[\.!?](?=\s|$)", text)
+
+    print(sentences)
