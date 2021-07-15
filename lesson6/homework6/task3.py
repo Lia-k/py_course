fae7ade7032c4aace147cb395c68437d01ee5998
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-07-15 19:10:20 +0300

    HW6: completed task3

---
+++ lesson6/homework6/task3.py
@@ -0,0 +1,13 @@
+# 3. Написать функцию is_prime, принимающую 1 аргумент — число от 2 до 1000,
+# и возвращающую True, если оно простое, и False - иначе.
+from typing import Union
+
+
+def is_prime(a: int) -> Union[bool, str]:
+    if a < 2 or a > 1000:
+        return "Please, enter a number in range 2-1000 inclusive"
+    else:
+        for i in range(2, a):
+            if a % i == 0:
+                return False
+        return True
