28b2a9144e896a98e05c2a6406706798a927c6f7
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-07-27 18:07:33 +0300

    HW9: completed task1

---
+++ lesson9/homework9/task1/__init__.py
@@ -0,0 +1,5 @@
+from .arithmetic_operations import subtraction
+from .arithmetic_operations import addition
+from .arithmetic_operations import multiplication
+from .arithmetic_operations import division
+from .arithmetic_operations import exponentiation
