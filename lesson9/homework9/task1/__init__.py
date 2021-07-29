4cf16d25c358057ffb64d6b146c80adf0ab00c30
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-07-30 00:21:28 +0300

    HW9: made changes

--- lesson9/homework9/task1/__init__.py
+++
@@ -1,5 +0,0 @@
-from .arithmetic_operations import subtraction
-from .arithmetic_operations import addition
-from .arithmetic_operations import multiplication
-from .arithmetic_operations import division
-from .arithmetic_operations import exponentiation
--- lesson9/homework9/task1/my_task.py
+++ lesson9/homework9/task1/my_task.py
@@ -2,7 +2,7 @@
 # для арифметических операций и выстроите фасад из импортов. Хочу иметь
 # возможность импортировать кое какие функции из пакета но не все.
 
-from lesson9.homework9.task1 import subtraction
+from lesson9.homework9.task1.arithmetic_operations import subtraction
 
 
 print(subtraction(3, 1))
