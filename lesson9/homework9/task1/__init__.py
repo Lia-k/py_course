fab95d6b0819732553200fcf77d821237e32c72f
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-29 15:18:47 +0300

    HW9: add comment with issue

--- lesson9/homework9/task1/__init__.py
+++ lesson9/homework9/task1/__init__.py
@@ -3,3 +3,5 @@ from .arithmetic_operations import addition
 from .arithmetic_operations import multiplication
 from .arithmetic_operations import division
 from .arithmetic_operations import exponentiation
+# Do you sure that this init should provide packages outside since you are
+# using them in task 1 only?
