e2d3ebe1203af3fe3f60cce17535aa940fc8603a
Lia-k <59776690+Lia-k@users.noreply.github.com>
2021-08-22 10:54:10 +0300

    HW15: fixed instance

--- lesson15/homework15/government_singleton.py
+++ lesson15/homework15/government_singleton.py
@@ -4,8 +4,8 @@ from typing import Type
 def singleton(_class: Type):
 
     def inner(*args):
-        if not hasattr(_class, f"{_class.__name__}__instance"):
-            setattr(_class, f"{_class.__name__}__instance", _class(*args))
-        return getattr(_class, f"{_class.__name__}__instance")
+        if not hasattr(_class, f"_{_class.__name__}__instance"):
+            setattr(_class, f"_{_class.__name__}__instance", _class(*args))
+        return getattr(_class, f"_{_class.__name__}__instance")
 
     return inner
